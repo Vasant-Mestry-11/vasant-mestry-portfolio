de4be8395988c5e5bd18b176aedcec676fe968b6
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-01 19:16:33 +0530

    :zap: :art: About me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./sections/Header";
 import Footer from "./sections/Footer";
 import Skills from "./sections/Skills";
+import AboutMe from "./sections/AboutMe";
 
 function App() {
   return (
     <>
       <Header />
       <Skills />
+      <AboutMe />
       <Footer />
     </>
   );
