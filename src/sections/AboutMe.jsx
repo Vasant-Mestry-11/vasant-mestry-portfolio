bfba949d8c4cce7dc955887ad5f2968326af57bd
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-02 20:57:48 +0530

    :zap: :lipstick: About me text

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -6,29 +6,29 @@ const AboutMe = () => {
       <img src={AboutMeIcon} />
       <div className="flex flex-col gap-5">
         <VariableWeightText regularText="About" extraBoldText="Me" />
-        <div className="text-neutral flex flex-col gap-5">
-          <p>
-            I'm a passionate, self-proclaimed designer who specializes in full
+        <div className="text-zinc-500 flex flex-col gap-5">
+          <p className="text-xl">
+            {`I'm a passionate, self-proclaimed designer who specializes in full
             stack development (React.js & Node.js). I am very enthusiastic about
             bringing the technical and visual aspects of digital products to
             life. User experience, pixel perfect design, and writing clear,
-            readable, highly performant code matters to me.
+            readable, highly performant code matters to me.`}
           </p>
-          <p>
-            I began my journey as a web developer in 2015, and since then, I've
+          <p className="text-xl">
+            {`I began my journey as a web developer in 2015, and since then, I've
             continued to grow and evolve as a developer, taking on new
             challenges and learning the latest technologies along the way. Now,
             in my early thirties, 7 years after starting my web development
             journey, I'm building cutting-edge web applications using modern
             technologies such as Next.js, TypeScript, Nestjs, Tailwindcss,
-            Supabase and much more.
+            Supabase and much more.`}
           </p>
-          <p>
-            When I'm not in full-on developer mode, you can find me hovering
+          <p className="text-xl">
+            {`When I'm not in full-on developer mode, you can find me hovering
             around on twitter or on indie hacker, witnessing the journey of
             early startups or enjoying some free time. You can follow me on
             Twitter where I share tech-related bites and build in public, or you
-            can follow me on GitHub.
+            can follow me on GitHub.`}
           </p>
         </div>
       </div>
