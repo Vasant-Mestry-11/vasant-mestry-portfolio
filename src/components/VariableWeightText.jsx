7ff4e87e3516263774ec72a13cb5e90055cd07f7
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-03 19:27:01 +0530

    :zap: :lipstick: :iphone: Hero banner UI

--- src/components/VariableWeightText.jsx
+++ src/components/VariableWeightText.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 const VariableWeightText = ({ invert = false, regularText, extraBoldText }) => {
   return (
     <h2
-      className={`text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl${
+      className={`text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl ${
         invert && "invert"
       }`}
     >
