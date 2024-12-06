fcaa306db1289c44fe28b4d317e145759b973d73
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-06 20:04:46 +0530

    :zap: :lipstick: :iphone: Hero banner

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,41 +8,51 @@ import InstagramIcon from "../assets/instagram.svg";
 
 const Hero = () => {
   return (
-    <section className="grid md:grid-cols-2 gap-5 relative">
-      <div className="flex flex-col gap-5 order-2 md:order-1">
-        <VariableWeightText
-          regularText={`Hello I'm`}
-          extraBoldText="Vasant Mestry ."
-        />
-        <VariableWeightText extraBoldText="Frontend Developer" regularText="" />
-        <VariableWeightText regularText={`Based In`} extraBoldText="Mumbai ." />
-        <p className="text-zinc-500 mt-8 text-xl">
-          {`I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
+    <section className="flex flex-col-reverse md:flex-row gap-10 items-center">
+      <div className="flex flex-col-reverse md:flex-row gap-10">
+        <div className="flex flex-col gap-8 md:w-[70%]">
+          <div className="flex flex-col gap-3">
+            <VariableWeightText
+              regularText={`Hello I'm`}
+              extraBoldText="Vasant Mestry ."
+            />
+            <VariableWeightText
+              extraBoldText="Frontend Developer"
+              regularText=""
+            />
+            <VariableWeightText
+              regularText={`Based In`}
+              extraBoldText="Mumbai ."
+            />
+          </div>
+          <p className="text-zinc-500 mt-2 text-md">
+            {`I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
           typesetting industry. Lorem Ipsum has been the industry's standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to specimen book.`}
-        </p>
-      </div>
-      <div className="order-1 md:order-2">
-        <img src={HeroImage} alt="hero" />
-      </div>
-      <div className="absolute bottom-16 left-20 flex gap-6">
-        <IconButton
-          icon={GithubIcon}
-          link={`https://github.com/Vasant-Mestry-11`}
-        />
-        <IconButton
-          icon={LinkedInIcon}
-          link={`https://www.linkedin.com/in/vasant-mestry/`}
-        />
-        <IconButton
-          icon={HashnodeIcon}
-          link={`https://vasantmestry.hashnode.dev/`}
-        />
-        <IconButton
-          icon={InstagramIcon}
-          link={`https://www.instagram.com/_vasantmestry_/`}
-        />
+          </p>
+          <div className="mb-2 left-4 flex gap-6 ">
+            <IconButton
+              icon={GithubIcon}
+              link={`https://github.com/Vasant-Mestry-11`}
+            />
+            <IconButton
+              icon={LinkedInIcon}
+              link={`https://www.linkedin.com/in/vasant-mestry/`}
+            />
+            <IconButton
+              icon={HashnodeIcon}
+              link={`https://vasantmestry.hashnode.dev/`}
+            />
+            <IconButton
+              icon={InstagramIcon}
+              link={`https://www.instagram.com/_vasantmestry_/`}
+            />
+          </div>
+        </div>
+        <div>
+          <img src={HeroImage} alt="hero" />
+        </div>
       </div>
     </section>
   );
