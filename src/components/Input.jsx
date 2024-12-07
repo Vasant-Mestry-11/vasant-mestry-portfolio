e03b8858d3b9a51b7fe9ee9625b06e53c686d033
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-07 17:54:28 +0530

    :zap: :art: Contact component added Input boxes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,25 @@
 import PropTypes from "prop-types";
 
-const Input = ({ type, name, value, onChange, classNames }) => {
+const Input = ({
+  type,
+  name,
+  value,
+  onChange,
+  classNames,
+  placeholder,
+  ...props
+}) => {
   return (
     <input
-      className={`border-2 border-zinc-500 px-8 py-4 rounded text-primary
+      className={`border-2 border-zinc-500 px-8 py-4 rounded text-primary w-[100%]
               hover:bg-zinc-100 
                 focus:drop-shadow-md focus:border-neutral ${classNames}`}
       type={type}
       name={name}
       value={value}
       onChange={onChange}
+      placeholder={placeholder}
+      {...props}
     />
   );
 };
@@ -22,4 +32,5 @@ Input.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   classNames: PropTypes.string,
+  placeholder: PropTypes.string,
 };
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,9 +1,58 @@
+import { useState } from "react";
 import Input from "../components/Input";
+import VariableWeightText from "../components/VariableWeightText";
 
 const Contact = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    website: "",
+    help: "",
+  });
+
+  const { name, email, website, help } = details;
+
+  const handleInputChange = (e) => {
+    setDetails((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <section>
-      <Input />
+    <section className="flex">
+      <div className="flex flex-col gap-5 w-[100%]">
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          placeholder="Your name"
+        />
+        <Input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleInputChange}
+          placeholder="Email"
+        />
+        <Input
+          type="text"
+          name="website"
+          value={website}
+          onChange={handleInputChange}
+          placeholder="Your website (if exists)"
+        />
+        <Input
+          type="textarea"
+          name="help"
+          value={help}
+          onChange={handleInputChange}
+          rows={12}
+          cols={10}
+          placeholder="How can I help ?"
+        />
+      </div>
     </section>
   );
 };
