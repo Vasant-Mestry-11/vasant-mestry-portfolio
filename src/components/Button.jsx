ada0f56ee56edc4ade34ac07a884d892f7167eef
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-01-10 20:55:15 +0530

    :zap: :art: Download resume functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-const Button = ({ children, classNames }) => {
+const Button = ({ children, classNames, onClick }) => {
   return (
     <button
       className={`px-5 py-4 bg-primary text-secondary font-semibold rounded items-center 
       flex justify-center gap-2 cursor-pointer hover:bg-neutral
       ${classNames}
       `}
+      onClick={onClick}
     >
       {children}
     </button>
@@ -18,4 +19,5 @@ export default Button;
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   classNames: PropTypes.string,
+  onClick: PropTypes.func,
 };
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,10 +3,20 @@ import Logo from "../components/Logo";
 import downloadIcon from "../assets/download.svg";
 
 const Header = () => {
+  const downloadResume = () => {
+    const pdfUrl = "/src/assets/Vasant Mestry.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <header className="flex justify-between items-center">
       <Logo />
-      <Button>
+      <Button onClick={downloadResume}>
         Resume
         <img src={downloadIcon} alt="download" />
       </Button>
